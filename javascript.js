ffd40d1509ab36b000476da5946d86850664d6a5
Nemeth Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2025-08-20 14:47:06 +0200

    projektek, email küldés

--- javascript.js
+++ javascript.js
@@ -1,2 +1,21 @@
-function email(){
-    window.open('mailto:nemeth.csaba.bence@gmail.com?subject=subject&body=body');}
+
+(function(){
+   emailjs.init("DuOZjcdU_KFx6nfxG");
+})();
+
+function sendMail() {
+    var ids = ["name", "email", "subject", "message"];
+    emailjs.send("service_j2w2tfl","template_8csgr78",{
+        name: document.getElementById(ids[0]).value,
+        email: document.getElementById(ids[1]).value,
+        subject: document.getElementById(ids[2]).value,
+        message: document.getElementById(ids[3]).value
+    }).then(function(response){
+        alert("Üzenet elküldve!");
+        for (let i = 0; i < ids.length; i++) {
+            document.getElementById(ids[i]).value = "";
+        }
+    }, function(error){
+        alert("Hiba történt: " + error);
+    });
+}
